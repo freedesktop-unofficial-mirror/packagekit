ae6e7ec7c41db9dbbe55d036b0203a546455fef9
Robin Norwood <rnorwood@redhat.com>
2008-05-22 10:57:45 -0400

    We never want to show self.txt, since it contains '(x/y) packagename' instead of just 'packagename'.

--- backends/yum/helpers/yumBackend.py
+++ backends/yum/helpers/yumBackend.py
@@ -1498,11 +1498,7 @@ class DownloadCallback(BaseMeter):
         '''
         Get the name of the package being downloaded
         '''
-        if self.text and type(self.text) == type(""):
-            name = self.text
-        else:
-            name = self.basename
-        return name
+        return self.basename
 
     def updateProgress(self,name,frac,fread,ftime):
         '''
